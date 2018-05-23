2fa263b684436a75ee21490614203e80e727e515
Timothy Daley <timpdaley@gmail.com>
2018-05-23 20:43:07 +0000

    front page looks awesome

---
+++ myWebsite/app/controllers/say_controller.rb
@@ -0,0 +1,14 @@
+class SayController < ApplicationController
+  def hello
+    @time = Time.now
+  end
+
+  def goodbye
+    @files = Dir.glob('*')
+    
+    # <% @files.each do |file| %>
+    #   file name is: <%= file %>
+    # <% end %>
+    
+  end
+end
