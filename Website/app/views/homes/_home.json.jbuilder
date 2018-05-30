4a488a19bdb0319ffa1cb7c6f1e44c38595c46f7
Timothy Daley <timpdaley@gmail.com>
2018-05-30 19:01:19 +0000

    This better work.

--- Website/app/views/homes/_home.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! home, :id, : title, :description, :image_url, :contact_info, :created_at, :updated_at
-json.url home_url(home, format: :json)
--- Website/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-
-  root to: 'visitors#index'
-  resources :homes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "home/download_pdf"
-end
